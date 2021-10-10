7da609a475ae613bd204bbb50031393f5ca2a1ab
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2021-10-10 22:16:02 +0530

    Create 2..cpp

---
+++ milestone 1 submission/2..cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+   int n;
+	cout<<"Enter a no.:";
+	cin>>n;
+	cout<<"You entered:"<<n;
+
+   return 0;
+}
+
+/*   Enter a no.:7
+     You entered:7   */
