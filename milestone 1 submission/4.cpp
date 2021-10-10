95db46e86571d3f515fadf522597d2c742516311
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2021-10-10 22:23:05 +0530

    Create 4.cpp

---
+++ milestone 1 submission/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int a,b,c,d,sum;
+	cout<<"enter four numbers:";
+	cin>>a>>b>>c>>d;
+	sum=a+b+c+d;
+	cout<<"sum="<<sum;
+
+   return 0;
+}
+
+/*enter four numbers:6 7 6 7
+  sum=26*/
