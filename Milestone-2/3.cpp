645da46c07c8ba5bfebf67764f6144148f0f78b8
Dharana Sharma <88919299+Dharana-Sharma@users.noreply.github.com>
2021-11-03 17:02:27 +0530

    Update 3.cpp

--- Milestone-2/3.cpp
+++ Milestone-2/3.cpp
@@ -22,3 +22,7 @@ int main()
     }
     return 0;
 }
+/*1       2       buzz    4       fizz    buzz    7       8       buzz    fizz    11      buzz    13      14      FizzBuzz
+16      17      buzz    19      fizz    buzz    22      23      buzz    fizz    26      buzz    28      29      FizzBuzz
+31      32      buzz    34      fizz    buzz    37      38      buzz    fizz    41      buzz    43      44      FizzBuzz
+46      47      buzz    49      fizz*/
